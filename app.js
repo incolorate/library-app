a1aaf4d7e1a2284db25787f33dabd703119bcc35
incolorate <negrupaul59@gmail.com>
2023-02-11 10:23:40 +0200

    Some stylistic changes

--- app.js
+++ app.js
@@ -6,18 +6,16 @@ let addBookAuthor = document.querySelector("#author ");
 let addBookStatus = document.querySelector("#status");
 let addBook = document.querySelector("#add-book");
 // Tesst
-let newBook = new Book("You don't know JS", "Kyle S.", "Read");
-let newBook1 = new Book("You don't know JS 1", "Kyle S.", "Read");
-let newBook2 = new Book("You don't know JS 2", "Kyle S.", "Read");
-addBookToLibrary(newBook);
-addBookToLibrary(newBook1);
-addBookToLibrary(newBook2);
 
 // Create new book
-function Book(title, author, read) {
+function Book(title, author, read, id) {
   this.title = title;
   this.author = author;
   this.read = read;
+  this.id = id;
+  this.remove = function (id) {
+    delete myLibrary[id];
+  };
 }
 
 // Push book to myLibrary
@@ -25,25 +23,41 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+//
+
 // Display all books in my library
 function displayBooks(array) {
   for (i = 0; i < array.length; i++) {
     let bookTitle = document.createElement("p");
     let bookAuthor = document.createElement("p");
     let bookRead = document.createElement("p");
+    let removeButton = document.createElement("button");
+    let divForBook = document.createElement("div");
+    array[i].id = i;
     bookTitle.innerText = array[i].title;
     bookAuthor.innerText = array[i].author;
     bookRead.innerText = array[i].read;
-    booksContainer.appendChild(bookTitle);
-    booksContainer.appendChild(bookAuthor);
-    booksContainer.appendChild(bookRead);
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookRead);
+    bookContainer.appendChild(removeButton);
   }
 }
 
-// Add new book dynamically
+// Remove the book button
+function removeBook(x) {
+  myLibrary[x].remove(myLibrary[x].id);
+}
 
+// Add new book dynamically
 function createNewBook() {
-  let newBook = new Book(addBookTitle.value, addBookAuthor.value, "read");
+  let bookStatus = "";
+  if (addBookStatus.checked) {
+    bookStatus = "Read";
+  } else {
+    bookStatus = "Not read yet";
+  }
+  let newBook = new Book(addBookTitle.value, addBookAuthor.value, bookStatus);
   addBookToLibrary(newBook);
   displayBooks(myLibrary);
 }
